b59b74698e9c744eb3377a3875b9d9a85906f59d
drkoch <drkoch@gmail.com>
2020-02-02 08:35:12 -0500

    cleanup unnecessary directives, and scoping on class variables

--- Mario/MainWindow.xaml.cs
+++ Mario/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Drawing;
-using System.Windows.Forms;
 
 namespace Mario
 {
@@ -34,9 +22,9 @@ namespace Mario
         private int ScreenWidth = 1024;
         private int ScreenHeight = 768;
 
-        long last = 0;
+        private long last = 0;
 
-        long millisecondsBetweenFrames;
+        private long millisecondsBetweenFrames;
 
         public MainWindow()
         {
